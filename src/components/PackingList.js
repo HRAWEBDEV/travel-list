b45c8772f00c6524246b3d9be858befbc4850a10
hamidwise1 <hrawebdev@gmail.com>
2023-08-23 20:02:46 +0430

    add sorting

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import Item from './Item';
+
 const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
+ const [sortBy, setSortBy] = useState('input');
+ const sortedItems = [...items].sort((itemOne, itemTwo) => {
+  if (sortBy === 'input') {
+   return 0;
+  }
+  if (sortBy === 'description') {
+   return itemOne.description.localeCompare(itemTwo.description);
+  }
+  if (sortBy === 'packed') {
+   return Number(itemOne.packed) - Number(itemTwo.packed);
+  }
+  return 0;
+ });
  return (
   <div className='list'>
    <ul>
-    {items.map((item) => (
+    {sortedItems.map((item) => (
      <Item
       key={item.id}
       item={item}
@@ -12,6 +27,18 @@ const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
      />
     ))}
    </ul>
+   <div className='actions'>
+    <select
+     value={sortBy}
+     onChange={(e) => {
+      setSortBy(e.target.value);
+     }}
+    >
+     <option value='input'>sort by input</option>
+     <option value='description'>sort by description</option>
+     <option value='packed'>sort by packed</option>
+    </select>
+   </div>
   </div>
  );
 };
